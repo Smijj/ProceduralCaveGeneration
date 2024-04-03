15cce64c46ca11f905ff6c204f40344d9d55f0fc
Smijj <declan.smyth@hotmail.com>
2024-04-03 21:39:46 +0930

    Setup basic Cellular Automata algorithm to create random cave-like spaces within the grid

--- U.ProceduralCaveGeneration/Assets/DSmyth/Scripts/TerrainModule/MapGenerator.cs
+++ U.ProceduralCaveGeneration/Assets/DSmyth/Scripts/TerrainModule/MapGenerator.cs
@@ -13,6 +13,7 @@ namespace DSmyth.TerrainModule
 
         [SerializeField] private string m_Seed;
         [SerializeField] private bool m_UseRandomSeed;
+        [SerializeField] private int m_SmoothingIterations = 5;
 
         [Range(0,100)]
         [SerializeField] private int m_RandomFillPercent;
@@ -26,6 +27,10 @@ namespace DSmyth.TerrainModule
         private void GenerateMap() {
             m_Map = new int[m_Width, m_Height];
             RandomFillMap();
+
+            for (int i = 0; i < m_SmoothingIterations; i++) {
+                SmoothMap();
+            }
         }
 
         private void RandomFillMap() {
@@ -33,14 +38,54 @@ namespace DSmyth.TerrainModule
                 m_Seed = DateTime.Now.TimeOfDay.ToString();
             }
 
-            System.Random psuedoRandom = new System.Random(m_Seed.GetHashCode());
+            System.Random pseudoRandom = new System.Random(m_Seed.GetHashCode());
 
             for (int x = 0; x < m_Width; x++) {
                 for (int y = 0; y < m_Height; y++) {
-                    m_Map[x, y] = psuedoRandom.Next(0, 100) < m_RandomFillPercent ? 1 : 0;
+                    if (x == 0 || x == m_Width-1 || y == 0 || y == m_Height-1) {
+                        m_Map[x, y] = 1;
+                    } else {
+                        m_Map[x, y] = pseudoRandom.Next(0, 100) < m_RandomFillPercent ? 1 : 0;
+                    }
                 }
             }
         }
+
+        private void SmoothMap() {
+            for (int x = 0; x < m_Width; x++) {
+                for (int y = 0; y < m_Height; y++) {
+                    int neighbourWallTiles = GetSurroundingWallCount(x, y);
+
+                    if (neighbourWallTiles > 4) {
+                        m_Map[x, y] = 1;
+                    } else if (neighbourWallTiles < 4) {
+                        m_Map[x, y] = 0;
+                    }
+                }
+            }
+        }
+
+        private int GetSurroundingWallCount(int gridX, int gridY) {
+            int wallCount = 0;
+
+            for (int neighbourX = gridX-1; neighbourX <= gridX + 1; neighbourX++) {
+                for (int neighbourY = gridY - 1; neighbourY <= gridY + 1; neighbourY++) {
+
+                    // Dont look at tiles out of the bounds of the m_Map array
+                    if (neighbourX < 0 || neighbourX >= m_Width || neighbourY < 0 || neighbourY >= m_Height) {
+                        wallCount++;    // if it is out of bound, add to wall count anyway to encourage wall growth
+                        continue;       
+                    }
+                    if (neighbourX == gridX && neighbourY == gridY) continue;   // Dont look at original tile
+
+                    wallCount += m_Map[neighbourX, neighbourY];
+                }
+            }
+
+            return wallCount;
+        }
+
+
 
         private void OnDrawGizmos() {
             if (!Application.isPlaying || m_Map == null) return;
